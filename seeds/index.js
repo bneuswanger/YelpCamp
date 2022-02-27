2dc7764eb93cb9b43418d8960ec533af90fdf86a
Bryan Neuswanger <bneuswanger@gmail.com>
2022-02-27 12:42:58 -0600

    styling

--- seeds/index.js
+++ seeds/index.js
@@ -21,9 +21,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae accusantium reiciendis nobis, deserunt voluptatum libero incidunt molestias aspernatur itaque omnis, quam nesciunt optio in quo recusandae voluptatibus placeat nulla exercitationem.',
+            price
         });
         await camp.save();
     }
